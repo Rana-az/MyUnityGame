ee4869eb78b8bd9bda90434cc8836faf0beb0fd2
Rana-az <140128656+Rana-az@users.noreply.github.com>
2025-09-15 17:08:48 +0300

    #Added jump code to MineScript

--- Assets/MineScript.cs
+++ Assets/MineScript.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class MineScript : MonoBehaviour
 {
+    // test change
+
     public Rigidbody2D myRigidbody;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
